7359eeab9fd6e163dd8cfcac6db069388c6bd498
Equinlo20 <elianquinto0@gmail.com>
2023-12-06 20:55:56 -0500

    feat: view details purchase work

--- src/main/java/org/openjfx/gamestore/controllers/Item_detail_purchaseController.java
+++ src/main/java/org/openjfx/gamestore/controllers/Item_detail_purchaseController.java
@@ -9,8 +9,11 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
+import org.openjfx.gamestore.models.domain.ItemGame;
+import org.openjfx.gamestore.utils.Utilities;
 
 public class Item_detail_purchaseController implements Initializable {
 
@@ -42,4 +45,13 @@ public class Item_detail_purchaseController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
     }    
     
+    public void setData(ItemGame item, int num){
+        this.numItem.setText(String.valueOf(num));
+        this.nameGameLabel.setText(item.getGame().getName());
+        this.descriptionGameTxt.setText(item.getGame().getDescription());
+        this.priceGameLabel.setText(String.valueOf(item.getGame().getPrice()));
+        this.amountGameLabel.setText(String.valueOf(item.getAmount()));
+        this.totalPriceItemLabel.setText("$" + item.getTotalItemPrice());
+        this.imageGame.setImage(new Image(Utilities.getUrlImage(item.getGame().getImgSrc())));
+    }
 }
--- src/main/java/org/openjfx/gamestore/controllers/View_purchaseController.java
+++ src/main/java/org/openjfx/gamestore/controllers/View_purchaseController.java
@@ -4,15 +4,21 @@
  */
 package org.openjfx.gamestore.controllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import org.openjfx.gamestore.data.LList;
+import org.openjfx.gamestore.models.domain.ItemGame;
 import org.openjfx.gamestore.models.domain.Purchase;
 import org.openjfx.gamestore.utils.ListenerProvider;
+import org.openjfx.gamestore.utils.Utilities;
 import org.openjfx.gamestore.utils.ViewPurchaseListener;
 
 public class View_purchaseController implements Initializable {
@@ -54,6 +60,31 @@ public class View_purchaseController implements Initializable {
         this.datePurchaseLabel.setText(purchase.getDate());
         this.numItemsLabel.setText(String.valueOf(purchase.getNumItems()));
         this.totalPaidLabel.setText("$" + purchase.getTotal());
+        
+        LList<ItemGame> items = purchase.getItems();
+        
+        for (int i = 0; i < items.getSize(); i++) {
+            try {
+                FXMLLoader fxmlLoader = new FXMLLoader();
+                fxmlLoader.setLocation(Utilities.getUrlFxmlResource("item_detail_purchase"));
+                
+                HBox hBox = fxmlLoader.load();
+                if (i%2==0) {
+                    hBox.getStyleClass().add("alternative-color");
+                }
+                Item_detail_purchaseController itemdpC = fxmlLoader.getController();
+                
+                try {
+                    itemdpC.setData(items.get(i), i+1);
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+                
+                purchasePaneTable.getChildren().add(hBox);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
     }
     
 }
